c165e878ead8094f739c0a92ca9cf52f73553487
doobybooby <doobinl0003@gmail.com>
2021-01-24 03:47:03 -0500

    added a info card

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Header from './componenets/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './componenets/navBar'
 import ImgMediaCard from './componenets/landingPage';
+import InteractiveList from './componenets/materialList';
+import Info from './componenets/infoPage';
 
 function App() {
   return (
     <div>
       <NavBar/> 
       <ImgMediaCard/>
+      <InteractiveList/>
+      <Info/>
 
     </div>
 
---
+++ src/componenets/infoPage/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import '../../css/infoPage.css'
+export default class Info extends Component {
+    render() {
+        return (
+            <div className="vn_item">
+                <div className="vn_outer">
+                    <div className="vn_inner">
+                        <div className="vn_icon_square bck_red">
+
+                        </div>
+                        <div 
+                            className="vn_icon"
+                            style={{
+                                }}
+                        >
+
+                        </div>
+                        <div className="vn_title">
+                            Event Date & Time
+                        </div>
+                        <div className="vn_desc">
+                            1 April 2017 @1.05 pm
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
