69593d06fcc80ade112ff50c8b0aaef9b833aaf3
doobybooby <doobinl0003@gmail.com>
2021-01-23 23:44:20 -0500

    created react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
+import Header from './componenets/header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      className="App" 
+      style={{
+         height:"1500px",
+         background:'darkgreen'
+        }}
+    >
+      <Header/>
+
     </div>
   );
 }
