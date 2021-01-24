13e7cbffe3296c31b6673cad06e4a392562bd9a2
doobybooby <doobinl0003@gmail.com>
2021-01-24 03:19:31 -0500

    added nav bar and a component card

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import React, { Component } from 'react';
 
 import Header from './componenets/header';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './componenets/navBar'
+import ImgMediaCard from './componenets/landingPage';
+
 function App() {
   return (
-    <div 
-      className="App" 
-      style={{
-         height:"1500px",
-         background:'darkgreen'
-        }}
-    >
-      <Header/>
+    <div>
+      <NavBar/> 
+      <ImgMediaCard/>
+
     </div>
+
+    
   );
 }
 
